ea21a3ad3b30d006cfa01f9494d1ff0e9af4b44a
Lê Văn Duẩn <duan.lv0308@outlook.com>
2024-04-10 08:16:38 +0000

    lect14 completed

--- homework/lect14.c
+++ homework/lect14.c
@@ -9,6 +9,13 @@
 
 #include <stdio.h>
 #include <string.h>
+typedef struct day {
+    int day;
+    int month;
+} day_t;
+char srcFileC[256];
+day_t day[366];
+int minTemp[366], maxTemp[366], humidity[366];
 
 int mergeFile(const char* fileName1, const char* fileName2) {
     FILE* fp1 = fopen(fileName1, "r");
@@ -30,27 +37,100 @@ int mergeFile(const char* fileName1, const char* fileName2) {
     return 0;
 }
 
-int skipCmtC(const char* srcFile) {
-    FILE* f = fopen(srcFile, "a+t");
+int delCmtC(const char* srcFile) {
+    FILE* f = fopen(srcFile, "a+w");
+    FILE* cf = fopen("copy.c", "w");
     if(f == NULL) {
         printf("Can't open this file!\n");
         return -1;
     }
-    // char buff[100], src[10000];
-    // while(fscanf(f,"%[^\n]", buff)) {
-    //     printf("|%s|\n",buff);
-    // }
+    char buff[256];
+    while (fscanf(f,"%[^\n]\n", buff) != EOF) {
+        strcat(buff, "\n");
+        // printf("%s", buff);
+        for(int i = 0; i < strlen(buff); i++) {
+            if(buff[i] == '/' && buff[i+1] == '/') {
+                buff[i] = '\n';
+                buff[i+1] = '\0';
+            }
+        }
+        fputs(buff, cf);
+        memset(buff,100, sizeof(char));
+    }
     fclose(f);
-    printf("\n");
+    fclose(cf);
+    remove(srcFile);
+    rename("copy.c", srcFile);
     return 0;
 }
 
-int readWeatherNote(const char* srcFile) {
+int readWeatherNote(const char* logFile) {
+    char buff[256];
+    FILE *f = fopen(logFile, "r"); 
+    if(f == NULL) {
+        printf("Can't open this file\n");
+        return -1;  
+    }
+    int i = 0;
+    while(fscanf(f,"%d/%d %d-%d %d", &day[i].day, &day[i].month, &minTemp[i], &maxTemp[i], &humidity[i]) != EOF) {
+        // printf("%d/%d %d-%d %d\n", day[i].day, day[i].month, minTemp[i], maxTemp[i], humidity[i]);
+        i++;
+    }
+    return 0;
+}
+
+double monthTempAVG(int month) {
+    double avg = 0;
+    int count = 0;
+    for(int i = 0; i < 366; i ++) {
+        if(day[i].month == month) {
+            count++;
+            avg = avg + (double)(minTemp[i] + maxTemp[i])/2;
+        }
+    }
+    // printf("%d - %lf - %lf\n", count, avg, avg/count);
+    return avg/count;
+}
+
+int wetDriestMonth(int *wettest, int * driest) {
+    double avgMonth[13];
+    double wMonth = 0, dMonth = 0;
+    memset(avgMonth, 13, sizeof(double));
+    for(int month = 1; month < 13; month++) {
+        int  count = 0;
+        avgMonth[month] = 0;
+        for(int i = 0; i < 366; i++) {
+            if(day[i].month == month) {
+                count++;
+                avgMonth[month] = avgMonth[month] + (double)humidity[i];
+            }
+        }
+        avgMonth[month] = avgMonth[month]/(double)count;
+        printf("\t%d - count; %d - avg: %.3lf\n", month, count ,avgMonth[month]);
+
+        if(avgMonth[month] > wMonth) {
+            wMonth = avgMonth[month];
+        }
+        if(avgMonth[month] < dMonth || dMonth == 0) {
+            dMonth = avgMonth[month];
+        }
+    }
+    printf("\t\twet: %.3lf  dry: %.3lf\n", wMonth, dMonth);
+
+
+    for(int month = 1; month < 13; month++) {
+        if(avgMonth[month] == wMonth) {
+            *wettest = month;
+        }
+        if(avgMonth[month] == dMonth) {
+            *driest = month;
+        }
+    }
     return 0;
 }
 
 int main() {
-    int chose = -1;
+    int chose = -1, wettestMonth, driestmonth;
     while(1){
         printf("\n----------------------------------------------------------------------\n"
                        "1. Ghép hai tệp văn bản\n"
@@ -67,10 +147,17 @@ int main() {
                 mergeFile("F1.txt", "F2.txt");
                 break;
             case 2:
-                skipCmtC("srcC.c");
+                printf("\tNhập tên file nguồn C: ");
+                scanf("%s", srcFileC);
+                delCmtC(srcFileC);
                 break;
             case 3:
-                
+                readWeatherNote("weatherLog.txt");
+                for(int i = 1; i < 13; i++){
+                    printf("\tNhiệt độ trung bình tháng %d: %.3lf\n", i, monthTempAVG(i));
+                }
+                wetDriestMonth(&wettestMonth, &driestmonth);
+                printf("\tTháng hanh khô nhất là tháng %d\n\tTháng ẩm ướt nhất là tháng %d\n", driestmonth, wettestMonth);
                 break;
             default:
                 printf("Invalid!\n");
--- homework/srcC.c
+++ homework/srcC.c
@@ -1,19 +1,12 @@
-// Program to take age and height input
+ 
 #include <stdio.h>
-
 int main() {
-    
-  int age;
-  // double height;
-  
-  printf("Enter the age: ");
-  scanf("%d", &age);
-  
-  // printf("Enter the height: ");
-  // scanf("%lf", &height);
-  
-  printf("Age = %d", age);
-  // printf("\nHeight = %.1lf", height);
+int age;
 
-  return 0;
+printf("Enter the age: "); 
+scanf("%d", &age);
+
+printf("Age = %d", age);
+
+return 0;
 }
