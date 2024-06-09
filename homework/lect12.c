db2b2360084f72896307c6d8f6ca95bf60daba3e
Lê Văn Duẩn <duan.lv0308@outlook.com>
2024-06-09 10:25:18 +0000

    Thêm hàm catDauCachThua() hoàn thiện.

--- homework/lect12.c
+++ homework/lect12.c
@@ -15,6 +15,44 @@ int removeExtraSpacesStr(char *str) {
     return 0;
 }
 
+char* catDauCachThua(char* str) {
+    int i = 0, len = strlen(str);
+    int k = len-1;
+    while(str[k] == ' ') {
+        k--;
+    }
+    while(str[i] == ' ') {
+        i++;
+    }
+    str[k+1] = '\0';
+    str+=i;
+
+
+    len =  strlen(str);
+    k = 0;
+    int isSpace = 0;
+    char tmp[len];
+
+    for(i = 0; i < len; i ++) {
+        if(str[i] != ' ') {
+            isSpace = 0;
+            tmp[k] = str[i];
+            k++;
+        } else {
+            isSpace++;
+            if(isSpace == 1) {
+                tmp[k] = ' ';
+                k++;
+            }
+        }
+    }
+    tmp[k] = '\0';
+    strcpy(str, tmp);
+    // printf("'%s'\n", result);
+    return str;
+
+}
+
 int wordCountStr(char *str) {
     removeExtraSpacesStr(str);
     int count = 1;
--- test.c
+++ test.c
@@ -1,6 +1,48 @@
 #include <stdio.h>
+#include <string.h>
+
+
+char* catDauCachThua(char* str) {
+    int i = 0, len = strlen(str);
+    int k = len-1;
+    while(str[k] == ' ') {
+        k--;
+    }
+    while(str[i] == ' ') {
+        i++;
+    }
+    str[k+1] = '\0';
+    str+=i;
+
+
+    len =  strlen(str);
+    k = 0;
+    int isSpace = 0;
+    char tmp[len];
+
+    for(i = 0; i < len; i ++) {
+        if(str[i] != ' ') {
+            isSpace = 0;
+            tmp[k] = str[i];
+            k++;
+        } else {
+            isSpace++;
+            if(isSpace == 1) {
+                tmp[k] = ' ';
+                k++;
+            }
+        }
+    }
+    tmp[k] = '\0';
+    strcpy(str, tmp);
+    // printf("'%s'\n", result);
+    return str;
+
+}
 
 int main() {
-    printf("hello world\n");
+
+    char name[100] = "   Le    Duan   Hai  Nguyen    Duong    ";
+    printf("'%s'", catDauCachThua(name));
     return 0;
 };
